f4d3a9dcbf896810fb99fb7c5f93f0d3a74d9f26
toandao1705 <132965803+toandao1705@users.noreply.github.com>
2023-11-02 14:25:05 +0700

    [Thanh Toàn] - index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+include "view/component/header.php";
+
+
+if ((isset($_GET['act'])) && ($_GET['act'] != "")) {
+    $act = $_GET['act'];
+    switch ($act) {
+        case 'deals':
+            include "view/deals.php";
+            break;
+        case 'about':
+            include "view/about.php";
+            break;
+        case 'shop':
+            include "view/shop.php";
+            break;
+        
+
+        default:
+            include "view/home.php";
+            break;
+    }
+} else {
+    include "view/home.php";
+}
+include "view/component/footer.php";
