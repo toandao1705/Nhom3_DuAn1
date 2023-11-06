7dda207b621b388923a459d273781a7fa84e468d
toandao1705 <132965803+toandao1705@users.noreply.github.com>
2023-11-06 15:42:44 +0700

    [Thanh Toàn] - model

---
+++ model/pdo.php
@@ -0,0 +1,90 @@
+<?php
+function pdo_get_connection()
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=duanmau", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // echo "Kết nối thành công!";
+        return $conn;
+    } catch (PDOException $e) {
+        echo "Kết nối thất bại!: " . $e->getMessage();
+    }
+}
+
+//sql
+// sql,...,id
+//"insert into loai(ten_loai) values(?)","Laptop"
+// "update loai set ten_loai=? where ma_loai=?,"Laptop", "1"
+//"delete form loai where ma_loai=?","1"
+
+
+//thêm sửa xóa dữ liệu
+function pdo_execute($sql)
+{
+    $sql_args = array_slice(func_get_args(), 1);
+    try {
+        $conn = pdo_get_connection();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($sql_args);
+    } catch (PDOException $e) {
+        throw $e;
+    } finally {
+        unset($conn);
+    }
+}
+
+function pdo_execute_return_lastInsertId($sql)
+{
+    $sql_args = array_slice(func_get_args(), 1);
+    try {
+        $conn = pdo_get_connection();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($sql_args);
+        return $conn->lastInsertId();
+    } catch (PDOException $e) {
+        throw $e;
+    } finally {
+        unset($conn);
+    }
+}
+
+
+//truy vấn nhiều dữ liệu
+function pdo_query($sql)
+{
+    $sql_args = array_slice(func_get_args(), 1);
+    try {
+        $conn = pdo_get_connection();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($sql_args);
+        $rows = $stmt->fetchAll();
+        return $rows;
+    } catch (PDOException $e) {
+        throw $e;
+    } finally {
+        unset($conn);
+    }
+}
+
+
+//truy vấn 1 dữ liệu
+function pdo_query_one($sql)
+{
+    $sql_args = array_slice(func_get_args(), 1);
+    try {
+        $conn = pdo_get_connection();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($sql_args);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $row;
+    } catch (PDOException $e) {
+        throw $e;
+    } finally {
+        unset($conn);
+    }
+}
