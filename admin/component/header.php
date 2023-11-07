8dcd78ecb4b996a1ddce3f5f4db39c2afbb4ee0a
toandao1705 <132965803+toandao1705@users.noreply.github.com>
2023-11-07 18:37:21 +0700

    [Thanh Toàn] sửa header và sidebar

--- admin/component/header.php
+++ admin/component/header.php
@@ -31,9 +31,9 @@
 <body class="hold-transition sidebar-mini layout-fixed">
   <div class="wrapper">
     <!-- Preloader -->
-    <div class="preloader flex-column justify-content-center align-items-center">
+    <!-- <div class="preloader flex-column justify-content-center align-items-center">
       <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-    </div>
+    </div> -->
 
     <!-- Navbar -->
     <nav class="main-header navbar navbar-expand navbar-white navbar-light">
@@ -165,7 +165,12 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
+          <a class="nav-link" href="index.php?act=logout" role="button">
+            <i class="fas fa-expand-arrows-alt"></i>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="index.php?act=logout" role="button">
             <i class="fas fa-th-large"></i>
           </a>
         </li>
