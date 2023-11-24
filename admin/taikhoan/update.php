54da52b237c4be2affbd1ddc318f04e1c7a06fee
toandao1705 <132965803+toandao1705@users.noreply.github.com>
2023-11-24 17:05:39 +0700

    [Thanh Toàn] - Bắt lỗi form update sp, tk

--- admin/taikhoan/update.php
+++ admin/taikhoan/update.php
@@ -1,5 +1,5 @@
-<?php 
-if(is_array($taikhoan)){
+<?php
+if (is_array($taikhoan)) {
   extract($taikhoan);
 }
 ?>
@@ -15,33 +15,34 @@ if(is_array($taikhoan)){
           </div>
           <!-- /.card-header -->
           <!-- form start -->
-          <form action="index.php?act=updatetk" method="post" enctype="multipart/form-data" onsubmit="return validateForm()">
+          <form action="index.php?act=updatetk" method="post" enctype="multipart/form-data" id="validateF">
             <div class="card-body">
               <div class="form-group">
                 <label for="exampleInputPassword1">Tên tài khoản</label>
-                <input type="text" class="form-control" name="name" value="<?php echo $name ?>" disabled>
+                <input type="text" class="form-control" name="name" id="name" value="<?php echo $name ?>" disabled>
               </div>
               <div class="form-group">
                 <label for="exampleInputPassword1">Email</label>
-                <input type="text" class="form-control" name="email" value="<?= $email ?>">
+                <input type="text" class="form-control" name="email" id="email" value="<?= $email ?>">
                 <span id="email-error" class="error-text text-danger"></span>
               </div>
               <div class="form-group">
                 <label for="exampleInputPassword1">Mật khẩu</label>
-                <input type="text" class="form-control" name="pass" value="<?= $pass ?>">
+                <input type="text" class="form-control" name="pass" id="pass" value="<?= $pass ?>">
                 <span id="pass-error" class="error-text text-danger"></span>
               </div>
               <div class="form-group">
                 <label for="exampleInputPassword1">Địa chỉ</label>
-                <input type="text" class="form-control" name="address" value="<?= $address ?>">
+                <input type="text" class="form-control" name="address" id="address" value="<?= $address ?>">
                 <span id="address-error" class="error-text text-danger"></span>
               </div>
               <div class="form-group">
                 <label for="exampleInputPassword1">Số điện thoại</label>
-                <input type="text" class="form-control" name="phone" value="<?= $phone ?>">
+                <input type="text" class="form-control" name="phone" id="phone" value="<?= $phone ?>">
                 <span id="tel-error" class="error-text text-danger"></span>
               </div>
             </div>
+
 
             <div class="card-footer">
               <input type="hidden" name="id" value="<?= $id ?>">
@@ -50,55 +51,69 @@ if(is_array($taikhoan)){
               <a href="index.php?act=listtk"><input type="button" value="DANH SÁCH"></a>
             </div>
           </form>
-
           <script>
-            function validateForm() {
-              var name = document.getElementsByName("name")[0].value;
-              var email = document.getElementsByName("email")[0].value;
-              var pass = document.getElementsByName("pass")[0].value;
-              var address = document.getElementsByName("address")[0].value;
-              var tel = document.getElementsByName("tel")[0].value;
-
-              var passPattern = /^.{6,}$/;
-              var telPattern = /^\d{10}$/;
+            // Hàm kiểm tra định dạng email
+            function isValidEmail(email) {
               var emailPattern = /\S+@\S+\.\S+/;
-
-              var isValid = true;
-
-              if (email.trim() === "") {
-                document.getElementById("email-error").textContent = "Email không được để trống";
-                isValid = false;
-              } else if (!email.match(emailPattern)) {
-                document.getElementById("email-error").textContent = "Email không đúng định dạng";
-                isValid = false;
-              } else {
-                document.getElementById("email-error").textContent = "";
-              }
-
-              if (!pass.match(passPattern)) {
-                document.getElementById("pass-error").textContent = "Mật khẩu phải có ít nhất 10 ký tự";
-                isValid = false;
-              } else {
-                document.getElementById("pass-error").textContent = "";
-              }
-
-              if (address.trim() === "") {
-                document.getElementById("address-error").textContent = "Địa chỉ không được để trống";
-                isValid = false;
-              } else {
-                document.getElementById("address-error").textContent = "";
-              }
-
-              if (!tel.match(telPattern)) {
-                document.getElementById("tel-error").textContent = "Số điện thoại phải có đúng 10 số";
-                isValid = false;
-              } else {
-                document.getElementById("tel-error").textContent = "";
-              }
-
-              return isValid;
+              return emailPattern.test(email);
             }
+
+            // Hàm kiểm tra định dạng số điện thoại
+            function isValidPhoneNumber(phone) {
+              var phonePattern = /^\d{10}$/;
+              return phonePattern.test(phone);
+            }
+
+            document.getElementById('validateF').addEventListener('submit', function(e) {
+              // Lấy giá trị của các trường dữ liệu
+              var email = document.getElementById('email').value;
+              var pass = document.getElementById('pass').value;
+              var address = document.getElementById('address').value;
+              var phone = document.getElementById('phone').value;
+
+              // Xóa thông báo lỗi cũ
+              document.getElementById('email-error').textContent = '';
+              document.getElementById('pass-error').textContent = '';
+              document.getElementById('address-error').textContent = '';
+              document.getElementById('tel-error').textContent = '';
+
+              // Bắt lỗi nếu email trống hoặc không đúng định dạng
+              if (email.trim() === '') {
+                e.preventDefault(); // Ngăn chặn việc submit form
+                document.getElementById('email-error').textContent = 'Email không được để trống';
+              } else if (!isValidEmail(email)) {
+                e.preventDefault(); // Ngăn chặn việc submit form
+                document.getElementById('email-error').textContent = 'Email không đúng định dạng';
+              }
+
+              // Bắt lỗi nếu mật khẩu trống hoặc không đúng định dạng
+              if (pass.trim() === '') {
+                e.preventDefault(); // Ngăn chặn việc submit form
+                document.getElementById('pass-error').textContent = 'Mật khẩu không được để trống';
+              } else if (pass.length < 6) {
+                e.preventDefault(); // Ngăn chặn việc submit form
+                document.getElementById('pass-error').textContent = 'Mật khẩu phải có ít nhất 6 ký tự';
+              }
+
+              // Bắt lỗi nếu địa chỉ trống
+              if (address.trim() === '') {
+                e.preventDefault(); // Ngăn chặn việc submit form
+                document.getElementById('address-error').textContent = 'Địa chỉ không được để trống';
+              }
+
+              // Bắt lỗi nếu số điện thoại trống hoặc không đúng định dạng
+              if (phone.trim() === '') {
+                e.preventDefault(); // Ngăn chặn việc submit form
+                document.getElementById('tel-error').textContent = 'Số điện thoại không được để trống';
+              } else if (!isValidPhoneNumber(phone)) {
+                e.preventDefault(); // Ngăn chặn việc submit form
+                document.getElementById('tel-error').textContent = 'Số điện thoại phải có đúng 10 số';
+              }
+            });
           </script>
+
+
+
 
         </div>
         <!-- /.card -->
